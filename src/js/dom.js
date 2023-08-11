029546def809022e817e0aa2985603cd5655921f
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-11 11:18:19 +0100

    Add button to add projects

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,11 +1,14 @@
 const populateAside = (projects) => {
     const aside = document.querySelector('aside');
+    const projectTabs = document.querySelector('#project-tabs');
 
     projects.forEach(project => {
         const title = document.createElement('p');
         title.textContent = project.name;
-        aside.appendChild(title);
+        projectTabs.appendChild(title);
     });
+    
+    aside.appendChild(projectTabs);
 }
 
 export { populateAside };
--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,4 @@ import { Todo } from './todo';
 import { getProjects, createProject } from './project';
 import { populateAside } from './dom';
 
-// get a list of projects and populate aside with it
-// ...
-createProject('Work');
 populateAside(getProjects());
