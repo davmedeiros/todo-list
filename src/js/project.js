0be4a92aabd063be4e84ef3eaa04441dfa4015ac
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-12 16:19:36 +0100

    Add localStorage for projects

--- src/js/project.js
+++ src/js/project.js
@@ -6,13 +6,24 @@ const Project = (name, items = []) => {
     return {  name, items, addItem };
 }
 
-const projects = [Project('Daily')];
+let projects = [];
+
+const saveToLocalStorage = () => {
+    localStorage.setItem("projects", JSON.stringify(projects));
+}
+
+const loadFromLocalStorage = () => {
+    projects = JSON.parse(localStorage.getItem("projects") || "[]");
+}
 
 const createProject = (name, ...items) => {
     projects.push(Project(name, items));
+    saveToLocalStorage();
 }
 
 const getProjects = () => {
+    loadFromLocalStorage();
+    projects.length === 0 && createProject('Daily');
     return projects;
 }
 
